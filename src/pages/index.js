4474158452fc7c7b60ad60efec73d719ef841069
Leanne Chen <lianchennnn@gmail.com>
2020-07-16 12:31:52 +0800

    feat(typography): install Typography plugin and setup config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
+import Container from "../components/container"
 
 export default function Home() {
   return (
-    <div>
+    <Container>
       <Header />
       <h1 style={{ color: `purple` }}>Hello Gatsby!</h1>
       <p>Hello world!</p>
       <Link to="/about/">About</Link>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
 
-    </div>
+    </Container>
   )
 }
