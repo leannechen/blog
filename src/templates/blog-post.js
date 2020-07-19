10662a038f812c53938f5dc4c3a5efc37ee3ffc5
Leanne Chen <lianchennnn@gmail.com>
2020-07-19 22:20:09 +0800

    feat(blog): append author section to blog template

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -8,10 +8,17 @@ export default function BlogPost({ data }) {
   return (
     <Layout>
       <SEO title={post.frontmatter.title} description={post.excerpt} />
-      <div>
+      <main>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
-      </div>
+      </main>
+      <hr/>
+      <footer>
+      {/* todo: About author image */}
+        <p>
+          <img src="../images/hamster-with-wheat-dumbbells.jpg" alt=""/>
+          Article by <Link to="/about/">Leanne</Link></p>
+      </footer>
     </Layout>
   )
 }
